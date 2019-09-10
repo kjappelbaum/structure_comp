f779b4b6beefdfd98aa4d19cd8e0c7b6fd939286
kjappelbaum <kevin.jablonka@epfl.ch>
2019-09-10 10:00:52 +0200

    simplify imports

--- structure_comp/__init__.py
+++ structure_comp/__init__.py
@@ -7,3 +7,8 @@ __email__ = 'kevin.jablonka@epfl.ch'
 __version__ = '0.1.0'
 __date__ = '18.03.19'
 __status__ = 'First Draft, Testing'
+
+from structure_comp.checker import Checker
+from structure_comp.cleaner import Cleaner
+from structure_comp.comparators import Statistics, DistStatistic, DistComparison, DistExampleComparison
+from structure_comp.sampling import Sampler
