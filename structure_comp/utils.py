fca81f584ebc91669ccaf83bfa83d71b9b96c76b
Kevin Jablonka <kevin.jablonka@epfl.ch>
2019-10-01 08:07:26 +0200

    Also returning indices, debugging

--- structure_comp/utils.py
+++ structure_comp/utils.py
@@ -222,7 +222,7 @@ def slugify(value, allow_unicode=False):
 def chunks(l, n):
     """ Yield successive n-sized chunks from l.
     """
-    for i in xrange(0, len(l), n):
+    for i in range(0, len(l), n):
         yield l[i : i + n]
 
 
